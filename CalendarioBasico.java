a7238ecc97ae5c9f47967f5373a5ec86193b0d5e
Nicolas Alfonso <nicalpucci@gmail.com>
2020-11-16 10:22:18 +0100

    0321.02 atributos dia, mes y año

---
+++ CalendarioBasico.java
@@ -0,0 +1,12 @@
+public class CalendarioBasico {
+    
+    private int dia;
+    private int mes;
+    private int ano;
+    
+    public CalendarioBasico(){
+        dia = 1;
+        mes = 1;
+        ano = 1;
+    }
+}
